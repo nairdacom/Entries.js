dc63e77a83c767c69de34a339c9aa4d986a15a51
nairdacom <mocadi@icloud.com>
2015-02-25 23:50:19 +0100

    Adapter test

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -25,6 +25,19 @@ var Main = function () {
     
       //debug mode
       console.log("\n\n-------- Debug Info --------\n\n");
+      var adapter = model.createAdapter('mongo', {
+        username: 'entries-js'
+        , dbname: 'heroku_app34321694'
+        , prefix: 'data/'
+        , password: 'GDRKZ27g'
+        , host: 'ds049161.mongolab.com'
+        , port: 49161
+      });
+      
+      adapter.connect(function (err) {
+          if (err) throw new Error('Error: ' + err);
+          console.log('Database connection successful');
+      }
       //debug mode
       
     User.first({id: this.session.get('userId')}, function (err, user) {
