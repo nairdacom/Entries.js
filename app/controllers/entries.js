4b4b643b1119f29cff4c735f71a5f5f61d394e4d
nairdacom <mocadi@icloud.com>
2015-03-20 13:55:51 +0100

    Entries > ParseCoaches FIX 6

--- app/controllers/entries.js
+++ app/controllers/entries.js
@@ -256,7 +256,7 @@ var Entries = function () {
           parseRowers(params.rower, function(parsedRowers){
 	          //utworzenie obiektu
 	          self.parseCoaches(params.coachList,function(list){ 
-	            var entryObj = geddy.model.Entry.create({rower:parsedRowers, event:self.event, competition:self.competition, user:self.user,coachList:params.coachList});
+	            var entryObj = geddy.model.Entry.create({rower:parsedRowers, event:self.event, competition:self.competition, user:self.user,coachList:list});
 	            entryObj.save(function(err,dat){ self.redirect("/entries/"+self.event.id); });
 	          });
           });
